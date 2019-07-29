5568451df74fa162c87af5a1ce274e31a94b5d51
chen song <chensong@eduroam-int-dhcp-97-77-251.ucl.ac.uk>
2019-07-29 11:51:02 +0100

    add patientid when adding answers

---
+++ auth/patientanswerAuth.js
@@ -0,0 +1,16 @@
+exports.authenticate = async (req, res, next) => {
+  var enforcer = await require('../config/casbin');
+  var role = []
+  while (role.length == 0){
+    await enforcer.getRolesForUser(String(req.models._id)).then((roles) => {
+      role = roles;
+    });
+  }
+  req.role = role;
+  next();
+}
+
+exports.new = async (req, res, next) => {
+  req.body.patient_id = req.jwt.id;
+  next();
+}
--- routes/v1/patientanswers.js
+++ routes/v1/patientanswers.js
@@ -1,13 +1,15 @@
 var router = require('express').Router();
+var patientAuth = require('../../auth/patientAuth');
 var patientanswerController = require('../../controller/v1/patientanswerController');
 
 router.route('/')
-    .get(patientanswerController.index)
-    .post(patientanswerController.new);
+  .get(patientanswerController.index)
+  .post(patientAuth.new, patientanswerController.new);
+
 router.route('/:id')
-    .get(patientanswerController.view)
-    .patch(patientanswerController.update)
-    .put(patientanswerController.update)
-    .delete(patientanswerController.delete);
+  .get(patientanswerController.view)
+  .patch(patientanswerController.update)
+  .put(patientanswerController.update)
+  .delete(patientanswerController.delete);
 
 module.exports = router;
