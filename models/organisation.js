ec4e504f67650b39a195600e765fd8043a37e873
chen song <chensong@eduroam-int-dhcp-97-38-167.ucl.ac.uk>
2019-08-06 15:42:04 +0100

    update remove organisation

--- models/organisation.js
+++ models/organisation.js
@@ -1,5 +1,6 @@
 var mongoose = require('../config/mongoose');
 var UserModel = require('../models/user')
+var OrganisationModel = require('../models/organisation')
 
 var Schema = mongoose.Schema;
 
@@ -48,9 +49,25 @@ var OrganisationSchema = new Schema({
 
 OrganisationSchema.post('remove', function(doc) {
   UserModel.deleteMany({
-    organisation_id: doc.organisation_id
+    organisation_id: doc._id
   }, function(err) {
     if (err) return handleError(err);
+  });
+
+  OrganisationModel.find({
+    organisation_id: doc._id
+  }).exec(function(err, models) {
+    var ids = [];
+    for (model of models) {
+      ids.push(model._id);
+    }
+    UserModel.deleteMany({
+      organisation_id: {
+        $in: ids
+      }
+    }, function(err) {
+      if (err) return handleError(err);
+    });
   });
 });
 
